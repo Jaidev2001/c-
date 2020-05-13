7c936c19c6ee46fe8e99a316178694aa5d011391
Jaidev2001 <jaidevbehera2001@gmail.com>
2020-05-13 18:09:16 +0530

    pointer testing

---
+++ cpp/pointer/ptrtoptr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char const* alphabet[26]
+    {
+        "alpha",
+        "bravo",
+        "charlie",
+        "delta",
+        "echo",
+        "foxtrot"
+    };
+
+    for (auto const** p = alphabet; *p != nullptr; ++p)
+    {
+        cout << *p << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
